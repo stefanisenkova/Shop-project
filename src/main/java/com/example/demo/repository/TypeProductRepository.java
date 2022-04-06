f4271368e8f7731cc7d28aa9e1980073e8ab48d2
Stanislav2509 <86559051+Stanislav2509@users.noreply.github.com>
2022-04-06 20:12:18 +0300

    Options buy something part one and search product by category

--- src/main/java/com/example/demo/repository/TypeProductRepository.java
+++ src/main/java/com/example/demo/repository/TypeProductRepository.java
@@ -1,7 +1,13 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.Employee;
+import com.example.demo.entity.Product;
 import com.example.demo.entity.TypeProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TypeProductRepository extends JpaRepository<TypeProduct,Integer> {
+    TypeProduct findByNameContainingIgnoreCase(String name);
+//    @Query("SELECT u FROM TypeProduct u WHERE u.name = ?1")
+//    public TypeProduct findByName(String type);
 }
