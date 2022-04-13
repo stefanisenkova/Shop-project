e1d008d86888b42a1b4d87562764e6dbedb6c857
Stanislav2509 <86559051+Stanislav2509@users.noreply.github.com>
2022-04-14 00:22:52 +0300

    update login

--- src/main/java/com/example/demo/repository/PurchaseRepository.java
+++ src/main/java/com/example/demo/repository/PurchaseRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.Employee;
+import com.example.demo.entity.User;
 import com.example.demo.entity.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
