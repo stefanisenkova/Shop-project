252d492d5ba75920072db28ff97cf66191819e58
Stefi <stefani.senkova1@gmail.com>
2022-04-17 17:51:41 +0300

    update

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,23 +1,21 @@
 package com.example.demo.controller;
 
 
-import com.example.demo.Status;
-import com.example.demo.entity.*;
-import com.example.demo.repository.*;
+import com.example.demo.entity.Role;
+import com.example.demo.entity.User;
+import com.example.demo.repository.RoleRepository;
+import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.validation.Valid;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Controller
 public class UserController {
@@ -102,7 +100,6 @@ public class UserController {
                 model.addAttribute("employees", employees);
 
             }
-
         }
         return "sorting-employees";
     }
@@ -120,4 +117,3 @@ public class UserController {
         return "sorting-employees";
     }
 }
-
